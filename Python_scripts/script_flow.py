acc7b1c8347bdbd4b8f4834bb9fb6748e95b1589
Darraghc <darragh-clerkin@hotmail.com>
2023-11-03 21:33:10 +0000

    updating regex find commands3

--- Python_scripts/script_flow.py
+++ Python_scripts/script_flow.py
@@ -33,6 +33,8 @@ def parse_string_data(json_string_data):
     """
     image_name = re.findall(IMAGE_NAME_PATTERN, str(json_string_data))
     image_name = str(set(image_name))
+    print("image_name_type: {0}".format(type(image_name)))
+    print(VERSION_PATTERN.format(image_name))
     image_version = re.findall(VERSION_PATTERN.format(image_name), str(json_string_data))
 
     results_data = re.findall(RESULTS_PATTERN, str(json_string_data))
