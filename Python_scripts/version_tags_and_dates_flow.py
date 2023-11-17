a8fbe4a058b6c1ce5d3deaf1ae0a7eef460db750
Darraghc <darragh-clerkin@hotmail.com>
2023-11-17 20:55:21 +0000

    trying to fix issue with node images version7

--- Python_scripts/version_tags_and_dates_flow.py
+++ Python_scripts/version_tags_and_dates_flow.py
@@ -8,7 +8,7 @@ version_release_dict = {}
 
 
 def get_version_tags():
-    repository_list = ["alpine", "nginx", "ubuntu", "python", "redis", "postgres", "node", "httpd", "memcached", "mongo",
+    repository_list = ["alpine", "nginx", "ubuntu", "redis", "postgres", "node", "httpd", "memcached", "mongo",
                         "mysql", "traefik", "mariadb", "docker", "rabbitmq", "golang", "wordpress", "php", "sonarqube", "ruby",
                           "haproxy", "tomcat", "kong", "neo4j", "amazonlinux", "caddy", "bash", "gradle", "plone", "fedora",
                           "groovy", "rust", "redmine", "amazoncorretto", "erlang", "elixir", "jruby", "jetty", "odoo", "xwiki",
