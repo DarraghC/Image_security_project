48539ff12dd798c2028151b692a5c52fed788a38
Darraghc <darragh-clerkin@hotmail.com>
2023-11-18 16:35:55 +0000

    Full run

--- Python_scripts/version_tags_and_dates_flow.py
+++ Python_scripts/version_tags_and_dates_flow.py
@@ -9,12 +9,11 @@ version_release_dict = {}
 
 #python mongo
 def get_version_tags():
-    repository_list = ["alpine", "nginx", "ubuntu"]
-                    #    , "redis", "postgres", "node", "httpd", "memcached", "python", "mongo",
-                    #     "mysql", "traefik", "mariadb", "docker", "rabbitmq", "golang", "wordpress", "php", "sonarqube", "ruby",
-                    #     "haproxy", "tomcat", "kong", "neo4j", "amazonlinux", "caddy", "bash", "gradle", "plone", "fedora",
-                    #     "groovy", "rust", "redmine", "amazoncorretto", "erlang", "elixir", "jruby", "jetty", "odoo", "xwiki",
-                    #     "swift", "haxe", "hylang", "archlinux", "tomee", "gcc", "monica", "varnish","orientdb", "julia"]
+    repository_list = ["alpine", "nginx", "ubuntu", "redis", "postgres", "node", "httpd", "memcached", "python", "mongo",
+                        "mysql", "traefik", "mariadb", "docker", "rabbitmq", "golang", "wordpress", "php", "sonarqube", "ruby",
+                        "haproxy", "tomcat", "kong", "neo4j", "amazonlinux", "caddy", "bash", "gradle", "plone", "fedora",
+                        "groovy", "rust", "redmine", "amazoncorretto", "erlang", "elixir", "jruby", "jetty", "odoo", "xwiki",
+                        "swift", "haxe", "hylang", "archlinux", "tomee", "gcc", "monica", "varnish","orientdb", "julia"]
 
     # Make a GET request to the Docker Hub API
     for image in repository_list:
